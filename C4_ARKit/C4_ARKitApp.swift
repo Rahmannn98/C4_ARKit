cf1c68925c7a5b41ff05e24781fd1b6c0dd8ae82
Rahmannn <saifulloh.rahman028@student.unud.ac.id>
2026-07-07 09:46:22 +0700

    Initial Commit

---
+++ C4_ARKit/C4_ARKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_ARKitApp.swift
+//  C4_ARKit
+//
+//  Created by Saifulloh Rahman on 07/07/26.
+//
+
+import SwiftUI
+
+@main
+struct C4_ARKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C4_ARKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C4_ARKit
+//
+//  Created by Saifulloh Rahman on 07/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
